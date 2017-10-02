b26a0b43c187692d9f34726fd0bab376ec4a5522
Prashanth Gowda <Prashanth_L_Gowda@dell.com>
2017-10-02 15:21:31 -0500

    Changes for API publish to bintray and github . Also versioning API change.

--- src/main/java/com/dell/isg/aps/powerthermal/ServicePowerThermalMonitoringApplication.java
+++ src/main/java/com/dell/isg/aps/powerthermal/ServicePowerThermalMonitoringApplication.java
@@ -3,6 +3,7 @@
  */
 package com.dell.isg.aps.powerthermal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,13 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import com.dell.isg.aps.powerthermal.common.UriConstants;
-import com.google.common.base.Predicates;
-
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -33,14 +28,12 @@ public class ServicePowerThermalMonitoringApplication {
         SpringApplication.run(ServicePowerThermalMonitoringApplication.class, args);
     }
 
+	@Autowired
+	private BuildInfo buildInfo;
+	
     @Bean
     public Docket anewsApipi() {
-        return new Docket(DocumentationType.SWAGGER_2).groupName("powerthermal").apiInfo(apiInfo()).select().paths(regex("/api.*")).build();
-    }
-
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("SMI Microservice :  Power and Thermal ").description("Power and Thermal Microservice collect power monitoring and consumption on compute node using industry standard WS-Management - WSMAN Protocol").termsOfServiceUrl("http://www.dell.com/smi/powermonitoring").license("Dell SMI License Version 1.0").licenseUrl("www.dell.com/smi").version("1.0 dev").build();
+        return new Docket(DocumentationType.SWAGGER_2).groupName("powerthermal").apiInfo(new ApiInfoBuilder().title("SMI Microservice :  Power and Thermal ").version(buildInfo.toString()).build()).select().paths(regex("/api.*")).build();
     }
 
 }
